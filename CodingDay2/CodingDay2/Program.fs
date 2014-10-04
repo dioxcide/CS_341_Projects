d3df628c1650bac11095e7924050e9c5b8fca430
Antonio Villarreal Jr. <clown_krown@yahoo.com>
2014-10-03 19:29:48 -0500

    Asd

---
+++ CodingDay2/CodingDay2/Program.fs
@@ -0,0 +1,109 @@
+﻿#light
+
+let UnitTest test = 
+  if test then
+    "correct"
+  else
+    "incorrect"
+
+
+// 
+// contains e L
+//
+let rec contains e L = 
+  if L = [] then
+    false
+  else if L.Head = e then
+    true
+  else 
+    contains e L.Tail
+
+
+// 
+// subset L1 L2
+//
+let rec subset L1 L2 = 
+  if L1 = [] then
+    true
+  else if (contains L1.Head L2) then
+    subset L1.Tail L2
+  else 
+    false
+
+
+//
+// multiply L1 L2
+//
+let rec multiply L1 L2 = 
+    if L1 = [] && L2 = [] then
+      ([] : int list)
+    else 
+       [(L1.Head * L2.Head)] @ multiply L1.Tail L2.Tail
+//
+// where e L
+//
+
+let rec find e L (index: int) = 
+    if L = [] then
+     0
+    else if L.Head = e then
+     index
+    else 
+     let temp = index + 1
+     find e L.Tail temp
+
+
+let where e L = 
+    find e L 1
+
+
+//
+// max L
+//
+let rec compMax (L: int list) currMax = 
+    if L = [] then
+        currMax
+    else if L.Head >= currMax then
+        compMax L.Tail L.Head
+    else 
+        compMax L.Tail currMax
+
+let max (L: int list) =
+    let  temp = compMax L L.Head
+    temp
+
+
+
+[<EntryPoint>]
+let main argv = 
+  printfn "contains: %A" (UnitTest ((contains 10 [2;88;6;44;10]) = true))
+  printfn "contains: %A" (UnitTest ((contains 11 [2;88;6;44;10]) = false))
+  printfn "contains: %A" (UnitTest ((contains 10 []) = false))
+
+  printfn ""
+
+  printfn "subset: %A" (UnitTest ((subset [44;2;88] [2;88;6;44;10]) = true))
+  printfn "subset: %A" (UnitTest ((subset [44;2;88;11] [2;88;6;44;10]) = false))
+  printfn "subset: %A" (UnitTest ((subset [] []) = true))
+
+  printfn ""
+
+  printfn "multiply: %A" (UnitTest ((multiply [1;2;3] [4;5;6] ) = [4;10;18]))
+  printfn "multiply: %A" (UnitTest ((multiply [-1;22;0;10] [-1;0;22;12]) = [1;0;0;120]))
+  printfn "multiply: %A" (UnitTest ((multiply [] []) = []))
+
+  printfn ""
+
+  printfn "where: %A" (UnitTest ((where 44 [2;88;6;44;10]) = 4))
+  printfn "where: %A" (UnitTest ((where 11 [2;88;6;44;10]) = 0))
+  printfn "where: %A" (UnitTest ((where 11 []) = 0))
+
+  printfn ""
+
+  printfn "max: %A" (UnitTest ((max [2;88;6;44;1;10]) = 88))
+  printfn "max: %A" (UnitTest ((max [-2;-88;-6;-44;-10]) = -2))
+  printfn "max: %A" (UnitTest ((max [11]) = 11))
+
+  printfn ""
+  printfn ""
+  0 // return success
--- hw3/hw3/hw3main.fs
+++ hw3/hw3/hw3main.fs
@@ -1,4 +1,13 @@
-﻿// Learn more about F# at http://fsharp.net
+﻿//
+// F# program to analyze Chicago rainfall data.
+//
+// Antonio Villarreal
+// U. of Illinois, Chicago
+// CS341, Fall 2014
+// Homework 3
+//
+
+// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 #light
 //
@@ -25,58 +34,103 @@ let PrintYearData (year:int, values:double list) =
  printfn ""
  printfn "%A: %A" year values
 
+ //Function computes the average for the given tuple
+let ComputeAvg (years:int, values: double list) =  
+    let avg = List.average values           //Computes the average
+    printf "%d: " years                     //Prints out the average for the year
+    printfn "%f" (avg)
 
-let ComputeAvg (years:int, values: double list) = 
-    let temp = snd(years, values)
-    let year = fst(years, values)
-    let avg = List.average temp
-    printf "%d: " year
-    printfn "%f\n" (avg)
-    0
-
-let rec ComputerMonthAvg (values:double list) index indexTracker = 
-    if index = indexTracker then
+//Function computes the sum of all the rainfall for a specific month
+let rec SpecificMonthRainFall (values:double list) index indexTracker = 
+    if index = indexTracker then //Returns the current value that we are currently at in the list
      values.Head
-    else
+    else                        //Increment the tracker to keep track of list position
      let currIndex = indexTracker + 1
-     ComputerMonthAvg values.Tail index currIndex
+     SpecificMonthRainFall values.Tail index currIndex //Recursive call to traverse the list
 
-let rec parseLinebyLine File Jan Feb Mar Apr May Jun July Aug Sep Oct Nov Dec Size = 
+// Prints all the data to the user
+let printData Jan Feb Mar Apr May Jun July Aug Sep Oct Nov Dec (yMax:int, mMax:int, Max:double) (yMin:int, mMin:int, Min:double) Size =
+    printfn "\n\tAverage Rainfall per Month\n" 
+    printfn "January: %A" (Jan/(float Size))
+    printfn "February: %A" (Feb/(float Size))
+    printfn "March: %A" (Mar/(float Size))
+    printfn "April: %A" (Apr/(float Size))
+    printfn "May: %A" (May/(float Size))
+    printfn "June: %A" (Jun/(float Size))
+    printfn "July: %A" (July/(float Size))
+    printfn "August: %A" (Aug/(float Size))
+    printfn "September: %A" (Sep/(float Size))
+    printfn "October: %A" (Oct/(float Size))
+    printfn "November: %A" (Nov/(float Size))
+    printfn "December: %A" (Dec/(float Size))
+    printfn "\n\tMax Rainfall"
+    printfn "%A, %A/%A" Max mMax yMax
+    printfn "\n\tMin Rainfall"
+    printfn "%A, %A/%A" Min yMin mMin
+
+//Function traverses the List and returns the index the item is at
+let rec traverseList (L:double list) item = 
+    if L = [] then                  //Base Case
+     0
+    else if L.Head = item then     //If the item is found
+     0
+    else                            //Recursively call itself until the item is found
+     1 + traverseList L.Tail item
+     
+//Finds the max number in the list and returns a tuple of the year month max value
+let findMax (L:double list) year (y:int, m:int, max:double) = 
+    let currMax = List.max L    //Finds max value
+
+    if max <= currMax then      //Compares it to the current maxvalue to see if it should be replaced
+        let month = traverseList L currMax + 1                  //Finds the month for the specific value
+        let (years, months, newMax) = (year, month,currMax)     //Creates a tuple of the month year and value
+        (years, months, newMax)                                 //returns the tuple
+
+    else
+        (y,m,max)              //If it shouldnt be replaced then it returns the original tuple
+
+//Function finds the minimum amount of rainfall
+let findMin (L:double list) year (y:int, m:int, min:double) =  
+    let currMin = List.min L        //Gets minimum amount of rainfall from current list
+
+    if min >= currMin then          //Compares it to the current min and creates a tuple if it needs to be updated
+        let month = traverseList L currMin + 1
+        let (years, months, newMin) = (year, month,currMin)
+        (years, months, newMin)     //Returns the tuple
+
+    else
+        (y,m,min)               //If the current min is smaller than anything in the list it will return the original tuple
+
+//Recursive function that reads in line the file list and calculates the averages max min rainfall
+let rec parseLinebyLine File Jan Feb Mar Apr May Jun July Aug Sep Oct Nov Dec (yMax:int, mMax:int, Max:double) (yMin:int, mMin:int, Min:double) Size = 
+    //Base case if the file is null then we print out the averages
     if File = [] then
-     printfn "January: %A" (Jan/(float Size))
-     printfn "February: %A" (Feb/(float Size))
-     printfn "March: %A" (Mar/(float Size))
-     printfn "April: %A" (Apr/(float Size))
-     printfn "May: %A" (May/(float Size))
-     printfn "June: %A" (Jun/(float Size))
-     printfn "July: %A" (July/(float Size))
-     printfn "August: %A" (Aug/(float Size))
-     printfn "September: %A" (Sep/(float Size))
-     printfn "October: %A" (Oct/(float Size))
-     printfn "November: %A" (Nov/(float Size))
-     printfn "December: %A" (Dec/(float Size))
-     []
-    else 
-     let (year, values) = ParseLine File.Head
-     
-     let rainfall = snd(year,values)
+     printData Jan Feb Mar Apr May Jun July Aug Sep Oct Nov Dec (yMax,mMax, Max) (yMin, mMin, Min) Size
 
-     let one  = Jan + ComputerMonthAvg rainfall 0 0
-     let two  = Feb + ComputerMonthAvg rainfall 1 0
-     let three  = Mar + ComputerMonthAvg rainfall 2 0
-     let four  = Apr + ComputerMonthAvg rainfall 3 0
-     let five  = May + ComputerMonthAvg rainfall 4 0
-     let six  = Jun + ComputerMonthAvg rainfall 5 0
-     let seven  = July + ComputerMonthAvg rainfall 6 0
-     let eigth  = Aug + ComputerMonthAvg rainfall 7 0
-     let nine  = Sep + ComputerMonthAvg rainfall 8 0
-     let ten  = Oct + ComputerMonthAvg rainfall 9 0
-     let eleven  = Nov + ComputerMonthAvg rainfall 10 0
-     let twelve  = Dec + ComputerMonthAvg rainfall 11 0
+     //[]
+    else 
+     let (year, values) = ParseLine File.Head       //Reads in the current line
+
+     ComputeAvg (year, values)                      //Computes the average for each year
+
+     let one  = Jan + SpecificMonthRainFall values 0 0  //Computes the sum for each month per year
+     let two  = Feb + SpecificMonthRainFall values 1 0
+     let three  = Mar + SpecificMonthRainFall values 2 0
+     let four  = Apr + SpecificMonthRainFall values 3 0
+     let five  = May + SpecificMonthRainFall values 4 0
+     let six  = Jun + SpecificMonthRainFall values 5 0
+     let seven  = July + SpecificMonthRainFall values 6 0
+     let eigth  = Aug + SpecificMonthRainFall values 7 0
+     let nine  = Sep + SpecificMonthRainFall values 8 0
+     let ten  = Oct + SpecificMonthRainFall values 9 0
+     let eleven  = Nov + SpecificMonthRainFall values 10 0
+     let twelve  = Dec + SpecificMonthRainFall values 11 0
+
+     let  (yMn, mMn, min) = (findMin values year (yMin, mMin, Min))     //Finds the max rainfall
+     let  (yMx, mMx, max) = (findMax values year (yMax, mMax, Max))     //Finds the min rainfall
      
-     ComputeAvg (year, values)
-     
-     parseLinebyLine File.Tail one two three four five six seven eigth nine ten eleven twelve Size
+     //Recursive call the function to compute the averages 
+     parseLinebyLine File.Tail one two three four five six seven eigth nine ten eleven twelve (yMx, mMx, max) (yMn, mMn, min) Size
 
 //
 // Main:
@@ -85,12 +139,13 @@ let rec parseLinebyLine File Jan Feb Mar Apr May Jun July Aug Sep Oct Nov Dec Si
 let main argv =
  // read entire file as list of strings:
  let file = ReadFile "rainfall-midway.txt"
- printfn "Average Rain Fall Per Year\n"
- parseLinebyLine file 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 (List.length file)
 
- //PrintYearData (year, values)
- // done:
- printfn ""
- printfn ""
+ printfn "\t**Rainfall Analysis Program **\n"
+ 
+ let (year, values) = ParseLine file.Head 
+ 
+ parseLinebyLine file 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 (0,0,0.0) (year,1,values.Head) (List.length file) 
+
+ printfn "** Done **"
  System.Console.ReadKey(true) |> ignore
  0 // return 0 => success
