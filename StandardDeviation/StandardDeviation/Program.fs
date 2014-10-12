f5ab77d2426e4ebe34c774bb76cc95d50909ee44
Antonio Villarreal Jr. <clown_krown@yahoo.com>
2014-10-12 13:56:49 -0500

    Swag

---
+++ StandardDeviation/StandardDeviation/Program.fs
@@ -0,0 +1,78 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+let rec _stddev L mean accum = 
+    match L with
+    | [] -> accum
+    | _  -> let head = L.Head 
+            let computation = head - mean
+            let squared = computation * computation
+            let accum2 = squared + accum
+            _stddev L.Tail mean accum2
+
+let stddev (L:float list) = 
+    let sum = List.sum L
+    let mean = sum/(float L.Length)
+    let addition = _stddev L mean 0.0
+    let fraction = (float addition) / (float L.Length)
+    let dev = System.Math.Sqrt(fraction)
+    printfn "%A" dev
+
+let rec tailElement (L) = 
+    match L with
+    | []    -> 0
+    | x::[] -> x
+    | _     -> tailElement(L.Tail)
+               
+    
+let absValue head: int = 
+    if head < 0 then
+        -head
+    else 
+        head
+
+let rec absList (L) tempList = 
+    match L with
+    | []    -> tempList
+    | _     -> if L.Head < 0 then
+                    let temp = tempList @ [-L.Head] 
+                    absList L.Tail temp
+               else 
+                    let temp = tempList @ [L.Head]
+                    absList L.Tail temp
+
+let rec absListT (L) = 
+    match L with
+    | []    -> []
+    | _     -> let temp2 = [absValue (L.Head)]
+               temp2 @ absListT L.Tail 
+
+let absHigherOrder (L) =
+    let temp = List.map absValue L
+    temp
+
+
+[<EntryPoint>]
+let main argv = 
+    let l = [2.0;4.0;4.0;4.0;5.0;5.0;7.0;9.0]
+    stddev l
+
+    let l2 = [9;8;7]
+    let temp = tailElement l2
+    printfn "%A" temp
+
+    let l3 = [0;-7;4]
+    let temp = absList l3 []
+    printfn "%A" temp
+
+    let temp = absListT l3 
+    printfn "%A" temp
+
+    let temp = absHigherOrder l3 
+    printfn "%A" temp
+
+    let temp = List.reduce (fun x y -> x+y) l3 
+    printfn "%A" temp
+
+    System.Console.ReadKey(true) |> ignore
+    0 // return an integer exit code
---
+++ WindowsFormsApplication1/PPMImageLibrary/ImageLibrary.fs
@@ -0,0 +1,62 @@
+﻿module PPMImageLibrary
+
+#light
+
+//
+// F#-based PPM image library.
+//
+// Antonio Villarreal
+// U. of Illinois, Chicago
+// CS341, Fall 2014
+// Homework 4
+//
+
+//
+// DebugOutput:
+//
+// Outputs to console, which appears in the "Output" window pane of
+// Visual Studio when you run with debugging (F5).
+//
+let rec private OutputImage(image:int list list) = 
+  match image with
+  | [ ] -> printfn "**END**"
+  |  _  -> printfn "%A" image.Head
+           OutputImage(image.Tail)
+           
+let DebugOutput(width:int, height:int, depth:int, image:int list list) =
+  printfn "**HEADER**"
+  printfn "W=%A, H=%A, D=%A" width height depth
+  printfn "**IMAGE**"
+  OutputImage(image)
+
+
+//
+// TransformFirstRowWhite:
+//
+// An example transformation: replaces the first row of the given image
+// with a row of all white pixels.
+//
+let rec BuildRowOfWhite cols white = 
+  match cols with
+  | 0 -> []
+  | _ -> // 1 pixel, i.e. RGB value, followed by remaining pixels:
+         white :: white :: white :: BuildRowOfWhite (cols-1) white
+
+let TransformFirstRowWhite(width:int, height:int, depth:int, image:int list list) = 
+  // first row all white :: followed by rest of original image
+  BuildRowOfWhite width depth :: image.Tail
+
+
+//
+// WriteP3Image:
+//
+// Writes the given image out to a text file, in "P3" format.  Returns true if successful,
+// false if not.
+//
+let WriteP3Image(filepath:string, width:int, height:int, depth:int, image:int list list) = 
+  //
+  //
+  // TODO!
+  //
+  //
+  true  // success
